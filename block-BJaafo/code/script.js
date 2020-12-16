fafbc2b6f2803755371942b409a876555a6850d4
artikgupta <artig9262@gmail.com>
2020-12-16 13:28:13 +0530

    complete block 1 of writing tests

---
+++ block-BJaafo/code/script.js
@@ -0,0 +1,23 @@
+function getFullName(firstName, lastName) {
+  let fullName = firstName + lastName;
+  return fullName;
+}
+let result = getFullName("Arti", "Gupta");
+
+let expected = "Arti Gupta";
+
+if (result !== expected) {
+  throw new Error(`${result} is not equal to ${expected}`);
+}
+
+let final = getFullName("Arti", "Gupta");
+
+let expected = "ArtiGupta";
+
+if (result !== expected) {
+  throw new Error(`${result} is not equal to ${expected}`);
+}
+
+function totalAmount(amount, taxRate) {
+  let totalAmt = amount + amount * taxRate;
+}
